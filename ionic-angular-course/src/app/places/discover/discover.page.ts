fdb1c241c9d728fd790e24fea653dff3478eaba3
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-07 15:47:53 +0200

    3 Lieux Factices

--- ionic-angular-course/src/app/places/discover/discover.page.ts
+++ ionic-angular-course/src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  // 2-Ici je vais ajouter mes Lieux chargés.
+  loadedPlaces: Place[];
+
+
+  // 1-Je vais injecter mon service Places dans l'attribut de mon constructor ( grace notamment à l'injectable qui se trouve dans le places.service (qui est mis automatiquement))
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    // 3 - je vais définir ce lieuchargé comme égal à ce service de lieux qui est une propriété de cette classe 
+    // car j'ai ajouté private devant l'argument du constructeur qui lie automatiquement cet argument 
+    // à une propriété privée de la classe portant le même nom et donc ici dans un service de lieux, 
+    // Je peux accéder aux lieux qui utilisent ce lieu getter que j'ai défini.
+    this.loadedPlaces = this.placesService.places;
   }
 
 }
--- ionic-angular-course/src/app/places/places.service.ts
+++ ionic-angular-course/src/app/places/places.service.ts
@@ -6,7 +6,32 @@ import { Place } from './place.model';
 })
 export class PlacesService {
 
-  private __places: Place[] = []; // Je précise que mes __places seront de types Place qui vient de mon modèle.
+  // Je précise que mes __places seront de types Place qui vient de mon modèle.
+  private __places: Place[] = [
+    // Je peux me créer des données fictives ici. 
+    new Place (
+      'P1',
+      'Manhattan Square',
+      'En plein coeur de New York City',
+      'https://r-cf.bstatic.com/images/hotel/max1024x768/403/40322050.jpg',
+      149.99
+    ),
+    new Place(
+      'P2',
+      'Suites Brando',
+      'Nouvelles suites à Bora Bora',
+      'https://pagtour.info/wp-content/uploads/2018/01/Capture-d%C2%B9%C3%A9cran-2018-01-09-%C3%A0-10.05.06.png',
+      549.85
+    ),
+    new Place(
+      'P3',
+      "Château d'Eltz",
+      "City-trip au chateau d'Eltz en Allemagne",
+      'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg',
+      549.85
+    ),
+    
+  ]; 
 
   get places() {
     // Donc ici je retourne une copie de mon tableau __places 
