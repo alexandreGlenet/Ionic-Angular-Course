4cd70b812f2e8d0c3f2fa6995979fef8b3a44a16
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-11 10:19:03 +0200

    Menu via une methode : onOpenMenu, & ctrl MenuControlleur

--- ionic-angular-course/src/app/places/discover/discover.page.ts
+++ ionic-angular-course/src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -14,7 +15,7 @@ export class DiscoverPage implements OnInit {
 
 
   // 1-Je vais injecter mon service Places dans l'attribut de mon constructor ( grace notamment à l'injectable qui se trouve dans le places.service (qui est mis automatiquement))
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
 
   ngOnInit() {
     // 3 - je vais définir ce lieuchargé comme égal à ce service de lieux qui est une propriété de cette classe 
@@ -22,6 +23,10 @@ export class DiscoverPage implements OnInit {
     // à une propriété privée de la classe portant le même nom et donc ici dans un service de lieux, 
     // Je peux accéder aux lieux qui utilisent ce lieu getter que j'ai défini.
     this.loadedPlaces = this.placesService.places;
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle();
   }
 
 }
