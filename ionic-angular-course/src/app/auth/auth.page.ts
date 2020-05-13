8d1a047963c5a0ccc6b7b73203eb3a15fd84e560
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-13 19:03:08 +0200

    fin du formulaire d'authentification

--- ionic-angular-course/src/app/auth/auth.page.ts
+++ ionic-angular-course/src/app/auth/auth.page.ts
@@ -43,7 +43,21 @@ export class AuthPage implements OnInit {
   };
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    //console.log(form);
+    if(!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if(this.isLogin) {
+      // Send a request to login servers
+
+    }
+    else {
+      //send a request to signup servers
+    }
   }
 
 }
