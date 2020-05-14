42774785036a39d5a5775992b95bed2d27cbbfa6
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-14 14:41:33 +0200

    Configuration des commandes de dates ( dont le random )

--- ionic-angular-course/src/app/places/places.service.ts
+++ ionic-angular-course/src/app/places/places.service.ts
@@ -14,21 +14,27 @@ export class PlacesService {
       'Manhattan Square',
       'En plein coeur de New York City',
       'https://r-cf.bstatic.com/images/hotel/max1024x768/403/40322050.jpg',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'P2',
       'Suites Brando',
       'Nouvelles suites à Bora Bora',
       'https://pagtour.info/wp-content/uploads/2018/01/Capture-d%C2%B9%C3%A9cran-2018-01-09-%C3%A0-10.05.06.png',
-      549.85
+      549.85,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'P3',
       "Château d'Eltz",
       "City-trip au chateau d'Eltz en Allemagne",
       'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg',
-      549.85
+      549.85,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     
   ]; 
